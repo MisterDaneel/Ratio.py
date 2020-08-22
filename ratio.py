f4bdbd01c678b25266e91ba6b60cc267f3db96ce
SirCipherz <sircipherz@mail.com>
2020-08-22 18:16:38 +0000

    Adding more arguments

--- ratio.py
+++ ratio.py
@@ -5,8 +5,13 @@ import sys
 
 def parse_args():
    """Create the arguments"""
-   parser = argparse.ArgumentParser('\nratio.py -c <configuration-file.json>')
+   parser = argparse.ArgumentParser()
+   
    parser.add_argument("-c", "--configuration", help="Configuration file")
+   parser.add_argument("-t", "--torrent", help="Torrent file")
+   parser.add_argument("-u", "--upload", help="Upload rate in kb/s")
+   parser.add_argument("-C", "--client", help="Client (transmission/qbittorrent)")
+   
    return parser.parse_args()
 
 def load_configuration(configuration_file):
@@ -16,6 +21,7 @@ def load_configuration(configuration_file):
     if 'torrent' not in configuration:
         return None
 
+    print(configuration)
     return configuration
 
 
@@ -23,9 +29,13 @@ if __name__ == "__main__":
     args = parse_args()
     if args.configuration:
         configuration = load_configuration(args.configuration)
+    elif (args.torrent != None) and (args.upload) and (args.client):
+        configuration = {'torrent': args.torrent,
+                         'upload': args.upload,
+                         'client': args.client
+           }
     else:
         sys.exit()
-   
     if not configuration:
         sys.exit()
  
